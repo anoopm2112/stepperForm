501a3921ea721ae9751819ff314b3e43901a3045
anoopm2112 <anoopm2112@gmail.com>
2022-03-15 16:11:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
 import React from 'react'
 import { Box, Grid, IconButton } from '@mui/material';
-import './App.css';
 import StepperForm from './components/StepperForm';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import CssBaseline from '@mui/material/CssBaseline';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
+import './App.css';
+
 const ColorModeContext = React.createContext({ toggleColorMode: () => { } });
 
 const ThemeSwitcher = () => {
   const theme = useTheme();
   const colorMode = React.useContext(ColorModeContext);
+
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        bgcolor: 'background.default',
-        color: 'text.primary',
-        borderRadius: 1,
-      }}
-    >
+    <Box sx={{ display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'center', bgcolor: 'background.default', color: 'text.primary', borderRadius: 1 }}>
       {theme.palette.mode} mode
       <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
         {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
@@ -31,27 +21,22 @@ const ThemeSwitcher = () => {
     </Box>
   );
 }
+
 function App() {
   const [mode, setMode] = React.useState('light');
 
-  const colorMode = React.useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-      },
-    }),
-    [],
-  );
+  const colorMode = React.useMemo(() => ({
+    toggleColorMode: () => {
+      setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+    },
+  }), []);
 
-  const theme = React.useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode,
-        },
-      }),
-    [mode],
-  );
+  const theme = React.useMemo(() => createTheme({
+    palette: {
+      mode,
+    },
+  }), [mode]);
+
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
